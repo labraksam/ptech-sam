95c4e9758fa7c38dd6c44a2a040918098b69e31c
Sam Hofman <sam@MacBook-Pro-van-Sam.local>
2020-03-19 16:36:55 +0100

    commit project tech

--- README.md
+++ README.md
@@ -1,1 +1,101 @@
-# Project Tech
+![alt text](https://github.com/labraksam/assessment-2.md/blob/master/screenshot.png "RelationshipGoals")
+
+
+# RelationshipGoals
+RelationShip goals is a site for people with different lifegoals to meet up. Its possible to register, log-in and find matches that have the samen lifegoals. Its also possible to upload your own profile picture, be in a session and delete your account.
+
+## How to install
+To install this, you have to follow a few steps.
+
+**Clone git**
+
+* Open your terminal and do ```cd``` with the name of the folder where you want to place it in.
+* Run the following code to clone it to the folder.
+
+```
+clone https://github.com/labraksam/be-assessment-2.git
+```
+
+**Install Mongodb**
+
+To save all the data you have to install Mongodb
+
+* First install Homebrew to help install Mongodb
+
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+```
+
+then install Mongodb
+
+```
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap mongodb/brew
+brew install mongodb-community@4.0
+```
+
+* In another tab in your terminal, you have to make a database folder with the following code.
+
+```
+mongod --dbpath db
+```
+
+Go back to the previous tab, and make a mongo database. Use two different tables. A ```user``` and ```lifegoals```.
+```
+mongo
+use relationshipgoals
+db.runCommand( { create: "users" } )
+db.runCommand( { create: "lifegoals" } )
+```
+
+* After creating it, you have to make sure the database is connected. ```cd``` to the main folder where you store the secret database info.
+```
+touch .env
+
+
+DB_HOST=localhost
+DB_USER=root
+DB_PORT=27017
+DB_NAME=relationshipgoals
+SESSION_SECRET=secret_message
+```
+
+Install npm to go further
+
+```
+npm install
+```
+
+**Run the server**
+
+To run the server, use nodemon. Use the following line in your terminal.
+```
+nodemon server/index.js
+```
+The website is now available at localhost:8080. Have a look!
+
+## Tech I used
+For the RelationshipGoals platform I used the following things
+
+* [NodeJS](https://nodejs.org/en/) is used to make the project in.
+
+* [Body-parser](https://www.npmjs.com/package/body-parser) to help with the request from the forms.
+
+* [Argon2](https://www.npmjs.com/package/argon2) to safe my password in a hesh (more safety).
+
+* [Multer](https://www.npmjs.com/package/multer) to upload an image in my form.
+
+* [Nodemon](https://github.com/remy/nodemon) makes it easier to use the server.
+
+* [Mongodb](https://www.mongodb.com/) the database to save data in.
+
+## To do's
+- [x] Make a working server
+- [x] Let users make an account, log in and log out
+- [x] Finding matches in the dashboard
+- [x] Delete function to delete your profile
+- [ ] More personal information to make matchs better
+- [ ] Lets a user chat with the match and sens pictures to eachother
+
+## Final words
+For me this was a tough course, but after all very interesting. In the beginning it was a lot of information that I didn't know what to do with. I like to work on something where you can see results. For me it was difficult to build something and see almost nothing change. After many more hours than expected, I was able to build what I wanted to build and finally saw some results. When this happened I was much more motivated to spend more hours on it and build the thing that I wanted to build. It is cool to see that I am building something that works, mainly because I started to think how I would do this. I have learned a lot from the theory, but especially the process itself.
---
+++ server/index.js
@@ -0,0 +1,277 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongo = require('mongodb')
+const argon2 = require('argon2')
+const session = require('express-session')
+const multer = require('multer')
+
+require('dotenv').config()
+
+let db = null
+
+// Create a new URL based on the .env credentials
+const url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
+
+// Make connection to the database
+mongo.MongoClient.connect(url, function(err, client) {
+  if (err) throw err
+  db = client.db(process.env.DB_NAME)
+})
+
+// Adds the uploaded picture to the specific folder 
+const upload = multer({
+  dest: 'static/upload/'
+})
+
+// Starts express, uses ejs files
+const app = express()
+  .set('view engine', 'ejs')
+  .set('views', 'src/view')
+  .use(express.static('static'))
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET
+  }))
+
+
+app.use('/images', express.static('src/images'))
+
+// Starts function
+  .get('/', home)
+  .get('/profile/:id', singleProfile)
+  .get('/signup', signup)
+  .post('/signup', upload.single('cover'), doSignup)
+  .post('/doLogin', doLogin)
+  .get('/dashboard', dashboard)
+  .get('/logout', logout)
+  .get('/myprofile', myprofile)
+//  .delete('/:id', remove)
+  .get('*', notFound)
+  .listen(8080)
+
+
+function dashboard(req, res) {
+
+  const data = {
+    session: req.session.user,
+    users: ''
+  }
+
+  if (data.session) {
+    console.log('ingelogd')
+    db.collection('users').find().toArray(done)
+
+    function done(err, allUsers) {
+      if (err) {} else {
+        console.log(data)
+        data.users = allUsers
+
+        res.render('dashboard/dashboard.ejs', data)
+      }
+    }
+  } else {
+    console.log('niet ingelogd')
+    res.render('dashboard/dashboard.ejs', data)
+  }
+}
+
+function home(req, res) {
+  const data = {
+    session: req.session.user
+  }
+
+  res.render('index.ejs', data)
+}
+
+function doLogin(req, res) {
+  const username = req.body.username
+  const password = req.body.password
+
+  if (!username || !password) {
+    res
+      .status(400)
+      .send('Username or password are missing')
+
+    return
+  }
+  const dbUsers = db.collection('users')
+
+  dbUsers.findOne({
+    email: username
+  }, function(err, user) {
+    if (err) {
+      console.log(err)
+    } else {
+      try {
+        argon2.verify(user.password, password)
+          .then(onverify)
+      } catch (err) {
+        res.redirect('index.ejs')
+        console.log(err)
+      }
+
+      function onverify(match) {
+       // Argon2 match with password
+          if (match) {
+          // Logged in
+          req.session.user = user
+
+          const data = {
+            session: req.session.user,
+            users: ''
+          }
+
+          res.redirect('dashboard')
+
+
+        } else {
+          res.status(401).send('Password incorrect')
+        }
+      }
+    }
+  })
+}
+
+function signup(req, res) {
+  const data = {
+    session: req.session.user
+  }
+
+  res.render('front/signup.ejs', data)
+}
+
+function doSignup(req, res) {
+
+  const currentUser = req.body.email
+  const password = req.body.password
+  const passwordAgain = req.body.passwordAgain
+  const min = 2
+  const max = 16
+
+  if (!currentUser || !password) {
+    res
+      .status(400)
+      .send('Username or password are missing')
+    return
+  }
+
+  if (password.length < min || password.length > max) {
+    res
+      .status(400)
+      .send(
+        'Password must be between ' + min +
+        ' and ' + max + ' characters'
+      )
+    return
+  }
+    
+  // Search for database table user
+  const users = db.collection('users')
+
+  // Search for a user
+  users.findOne({
+    // Search for input username
+    username: currentUser
+  }, function(err, user) {
+    if (err) {
+      console.log(err)
+    } else {
+      argon2.hash(password).then(onhash)
+    }
+  })
+
+    // Will add hash to database
+  function onhash(hash) {
+    db.collection('users').insertOne({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hash,
+      cover: req.file ? req.file.filename : null,
+      gender: req.body.gender,
+      gender_pref: req.body.gender_pref,
+      birthday: req.body.birthday,
+      age_pref_min: req.body.age_pref_min,
+      age_pref_max: req.body.age_pref_max
+    }, doneInserting)
+
+      // Going fo a sesion with user data
+    function doneInserting(err, user) {
+      req.session.user = user.ops[0]
+
+      const data = {
+        session: req.session.user
+      }
+    
+      res.redirect('dashboard')
+    }
+  }
+}
+
+function logout(req, res) {
+  req.session.destroy(function(err) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/')
+    }
+  })
+}
+
+function singleProfile(req, res) {
+  const data = {
+    session: req.session.user,
+    param: req.params.id,
+    currentUser: ''
+  }
+  if (data.session) {
+    console.log('render vanaf sessie')
+    const mongoID = new mongo.ObjectID(req.params.id)
+    db.collection('users').findOne({
+      _id: mongoID
+    }, function(err, user) {
+      if (err) {
+        console.log(err)
+      } else {
+        data.currentUser = user
+        res.render('dashboard/profile.ejs', data)
+      }
+    })
+  } else {
+
+    res.render('dashboard/profile.ejs', data)
+  }
+}
+
+function notFound(req, res) {
+  const data = {
+    session: req.session.user,
+    errorMessage: '404, pagina niet gevonden!'
+  }
+
+  res.render('front/error', data)
+}
+
+function myprofile(req, res) {
+  const data = {
+    session: req.session.user,
+  }
+
+  res.render('dashboard/myprofile.ejs', data)
+}
+
+//function remove(req, res) {
+//  const id = req.params.id
+//
+//  data = data.filter(function(value) {
+//    return user.id !== id
+//  })
+//
+//  res.json({
+//    status: 'ok'
+//  })
+//}
